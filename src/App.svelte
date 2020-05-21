7ed72342ccff6b718f14b761ce928de98db8f453
vpassanisi <vpassanisi@gmail.com>
2020-05-20 18:28:14 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="typescript">
+  import { darkMode } from "./stores/darkMode.js";
+  import { media } from "./stores/watchMedia.js";
+  import "./CSS/tailwind.css";
+
+  let title: string = " from Typescript";
+</script>
+
+<main class="h-screen bg-red-500 dark:bg-blue-500">
+  {#if $media.md}
+    <h1 class="text-white">Hello{title}</h1>
+  {:else}
+    <h1 class="text-white">Bye{title}</h1>
+  {/if}
+  <button on:click={() => ($darkMode ? darkMode.turnOff() : darkMode.turnOn())}>
+    darkmode
+  </button>
+</main>
